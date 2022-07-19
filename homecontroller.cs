f9c6a8709da12b9338ab3b17a093c2d87d76b489
ROHIT SINGH <36737110+funrohit@users.noreply.github.com>
2022-07-19 21:40:24 +0530

    Update homecontroller.cs

--- homecontroller.cs
+++ homecontroller.cs
@@ -39,11 +39,72 @@ namespace Udemy.Controllers
                 _db.mytables.Add(obj);
                 _db.SaveChanges();
 
+                TempData["suc"] = "Created SUCESSFULLY!!";
                 return RedirectToAction("Index");
 
             }
 
             return View("Create");
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int? id)
+        {
+            if(id == 0 || id == null)
+            {
+                return NotFound();  
+            }
+
+            var res = _db.mytables.Find(id);
+
+            if(res == null)
+            {
+                return NotFound();
+            }
+
+            return View(res);
+
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(mytable obj)
+        {
+
+            if (ModelState.IsValid)
+            {
+                _db.mytables.Update(obj);
+                _db.SaveChanges();
+                TempData["suc"] = "Edit SUCESSFULLY!!";
+                return RedirectToAction("Index");
+
+            }
+
+            return View("Create");
+        }
+
+
+        public IActionResult Del(int? id)
+        {
+            if (id == 0 || id == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                var res = _db.mytables.Find(id);
+
+                if (res == null)
+                {
+                    return NotFound();
+                }
+                _db.mytables.Remove(res);
+                _db.SaveChanges();
+                TempData["suc"] = "Deleted SUCESSFULLY!!";
+            }
+
+            return RedirectToAction("Index");
         }
     }
 }
